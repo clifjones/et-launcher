79dab36c1a006d9101c2cbab92bf4e795ae83d51
clif <jonect@gmail.com>
2025-04-12 03:44:16 +0000

    Initial commit

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,18 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+mod commands;
+
+fn main() {
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![
+            commands::run_app,
+            commands::read_et_mode,
+            commands::write_et_mode,
+            commands::read_user_config,
+            commands::write_user_config,
+            commands::read_active_radio
+        ])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
